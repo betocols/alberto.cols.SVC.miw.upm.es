df5f18d9084785a0fcbc0b530fdac77b69e8a5c8
albertocols <albertocols@192.168.1.46>
2016-10-02 20:53:48 +0200

    feature#01: Create class C12Test

---
+++ src/test/java/miw/iwvg/classes/C12Test.java
@@ -0,0 +1,20 @@
+package miw.iwvg.classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C12Test {
+	C12 c;
+	
+	@Before
+	public void before() {
+		c  = new C12();
+	}
+	
+	@Test
+	public void testMA() {
+		assertEquals("mA", c.mA());
+	}
+}
